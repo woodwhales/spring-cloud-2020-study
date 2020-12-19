938c9c7a4c1593ddbce3f63775b577d3f4553614
woodwhales <woodwhales@163.com>
2020-12-19 23:54:40 +0800

    added order module

---
+++ project/spring-cloud-project/cloud-consumer-order80/src/main/java/cn/woodwhales/springcloud/OrderMain80.java
@@ -0,0 +1,17 @@
+package cn.woodwhales.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author woodwhales
+ * @date 2020-12-19 22:18
+ */
+@SpringBootApplication
+public class OrderMain80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class, args);
+    }
+
+}
---
+++ project/spring-cloud-project/cloud-consumer-order80/src/main/java/cn/woodwhales/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,16 @@
+package cn.woodwhales.springcloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author woodwhales
+ */
+@Configuration
+public class ApplicationContextConfig {
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+}
