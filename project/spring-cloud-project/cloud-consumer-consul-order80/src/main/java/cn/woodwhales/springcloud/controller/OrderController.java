c0d39300444ce7458d61568d4c5790a5e79000d4
woodwhales <woodwhales@163.com>
2021-01-10 16:02:27 +0800

    added consul-order module

---
+++ project/spring-cloud-project/cloud-consumer-consul-order80/src/main/java/cn/woodwhales/springcloud/controller/OrderController.java
@@ -0,0 +1,29 @@
+package cn.woodwhales.springcloud.controller;
+
+import cn.woodwhales.springcloud.entity.CommonResult;
+import cn.woodwhales.springcloud.entity.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Controller
+@RequestMapping("/consumer")
+@RestController
+public class OrderController {
+
+    public static final String PAYMENT_URL = "http://cloud-provider-payment";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/payment/get/consul/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/consul/" + id, CommonResult.class);
+    }
+}
